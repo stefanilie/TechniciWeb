b8c0443e57e5ea67a80e43a09d73c92be8de7287
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-04-03 14:25:20 +0300

    terminat 6_1

--- Lab6/Tema6_1.js
+++ Lab6/Tema6_1.js
@@ -2,11 +2,8 @@ window.onload=function()
 {
 	var p1 = document.getElementById('p1');
 	var p2 = document.getElementById('p2');
-	p1.onclick = schimba(p2, 'blue');
-	p2.onclick = schimba(p1, 'red');
-}
-
-function schimba(p, s)
-{
-	p.style.color = s;
+	p1.onmouseover = function(){p2.style.color="red";}
+	p2.onmouseover = function(){p1.style.color="blue";}
+	p1.onmouseout = function(){p2.style.color = "black";}
+	p2.onmouseout = function(){p1.style.color = "black";}
 }
