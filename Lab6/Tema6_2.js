aeff2c12c4c32bef41f034a0d2dee2d485dbea53
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-04-06 22:19:12 +0300

    tren work

---
+++ Lab6/Tema6_2.js
@@ -0,0 +1,10 @@
+window.onload=function
+{
+	function addText()
+	{
+		var textToWrite = document.getElementById().value;
+		document.myform.textarea.value += textToWrite;
+	}
+
+	function change()
+}
